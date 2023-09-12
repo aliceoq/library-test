3b2b6824634703fdc25e2b3857e14efde41a68e4
Robson Oliveira <ropj@cin.ufpe.br>
2023-09-12 10:30:11 -0300

    chore: new release

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,10 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import json from "@rollup/plugin-json";
 import css from "rollup-plugin-import-css";
-
-const packageJson = require("./package.json");
 
 export default [
   {
