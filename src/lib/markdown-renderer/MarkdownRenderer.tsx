7a95fc6421a3fe47b437082723da4b078ee567f9
Robson Oliveira <ropj@cin.ufpe.br>
2023-09-15 09:56:48 -0300

    update: test tsup

--- src/lib/markdown-renderer/MarkdownRenderer.tsx
+++ src/lib/markdown-renderer/MarkdownRenderer.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import { Box } from '@vtex/brand-ui'
 import { MDXRemote } from 'next-mdx-remote'
 import { MarkdownRendererProps } from './MarkdownRenderer.types'
 import components from './components'
-import '@highlightjs/cdn-assets/styles/github.min.css'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const MDXRemote2 = MDXRemote as any
 
