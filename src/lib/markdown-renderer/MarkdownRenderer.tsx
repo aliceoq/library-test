5d84f57dccbd40f838efeff9efe0871f83b70669
Robson Oliveira <ropj@cin.ufpe.br>
2023-09-15 18:01:45 -0300

    n remove

--- src/lib/markdown-renderer/MarkdownRenderer.tsx
+++ src/lib/markdown-renderer/MarkdownRenderer.tsx
@@ -2,14 +2,16 @@ import { Box } from '@vtex/brand-ui'
 import { MDXRemote } from 'next-mdx-remote'
 import { MarkdownRendererProps } from './MarkdownRenderer.types'
 import components from './components'
-import '@highlightjs/cdn-assets/styles/github.min.css'
+import className from '@highlightjs/cdn-assets/styles/github.min.css'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const MDXRemote2 = MDXRemote as any
 
 const MarkdownRenderer = ({ serialized }: MarkdownRendererProps) => (
-  <Box>
-    <MDXRemote2 components={components} lazy {...serialized} />
-  </Box>
+  <div className={className}>
+    <Box>
+      <MDXRemote2 components={components} lazy {...serialized} />
+    </Box>
+  </div>
 )
 
 export default MarkdownRenderer
