4b5e318dc24e69c43cf5df9b50d1a6aeb31afb80
Robson Oliveira <ropj@cin.ufpe.br>
2023-09-18 18:37:31 -0300

    revert: last version

--- src/lib/markdown-renderer/MarkdownRenderer.tsx
+++ src/lib/markdown-renderer/MarkdownRenderer.tsx
@@ -2,16 +2,13 @@ import { Box } from '@vtex/brand-ui'
 import { MDXRemote } from 'next-mdx-remote'
 import { MarkdownRendererProps } from './MarkdownRenderer.types'
 import components from './components'
-import className from '@highlightjs/cdn-assets/styles/github.min.css'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const MDXRemote2 = MDXRemote as any
 
 const MarkdownRenderer = ({ serialized }: MarkdownRendererProps) => (
-  <div className={className}>
-    <Box>
-      <MDXRemote2 components={components} lazy {...serialized} />
-    </Box>
-  </div>
+  <Box>
+    <MDXRemote2 components={components} lazy {...serialized} />
+  </Box>
 )
 
 export default MarkdownRenderer
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,4 @@
 import { defineConfig } from 'tsup'
-// import css from "rollup-plugin-import-css"
-import scss from 'rollup-plugin-scss'
-import sass from 'sass'
-import styles from "rollup-plugin-styles";
-import { sassPlugin } from 'esbuild-sass-plugin'
-import postcss from 'postcss';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -13,12 +6,4 @@ export default defineConfig({
   clean: true,
   dts: true,
   format: 'esm',
-  esbuildPlugins: [
-    sassPlugin({
-      async transform(source) {
-          const { css } = await postcss([autoprefixer]).process(source);
-          return css;
-      },
-  }),
-  ]
 })
