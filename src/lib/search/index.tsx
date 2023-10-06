aa9fc004b3f4fb74976ff795c213d237771cf24d
aliceoq <aoqb@cin.ufpe.br>
2023-10-06 12:32:44 -0300

    fix: show searchInput in search page

--- src/lib/search/index.tsx
+++ src/lib/search/index.tsx
@@ -4,12 +4,15 @@ import SearchResults from 'components/search-results'
 import SearchFilterTabBar from 'components/search-filter-tab-bar'
 import SearchContextProvider from 'utils/context/search'
 import styles from './styles'
+import SearchInput from 'components/search-input'
 
 const Search = () => {
   return (
     <SearchContextProvider>
       <Box>
-        <Flex sx={styles.searchBarContainer}></Flex>
+        <Flex sx={styles.searchBarContainer}>
+          <SearchInput />
+        </Flex>
         <SearchFilterTabBar />
       </Box>
       <Flex sx={styles.body}>
